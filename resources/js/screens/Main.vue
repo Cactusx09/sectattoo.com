7ce92f22e7fe3d216933c079d6c2026e41f27e6f
trg <cactusx09@gmail.com>
2019-06-17 20:14:32 +0300

    [ADD] test Main screen component

---
+++ resources/js/screens/Main.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example </div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        mounted() {
+            axios.get('/api/v1/works').then((data) => { console.log(data); debugger;})
+        }
+    }
+</script>
