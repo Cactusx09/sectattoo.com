ee71eb4958058d7dc293b0535a97b8470eb57a12
trg <cactusx09@gmail.com>
2019-07-28 20:24:33 +0300

    [NPM] add animejs and inView packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,13 @@ import VueRouter from 'vue-router'
 import Routes from '@/js/routes/index.js'
 import App from '@/js/views/App'
 
+import anime from 'animejs'
+Object.defineProperty(Vue.prototype, '$anime', { value: anime })
+
+import inView from 'in-view'
+Object.defineProperty(Vue.prototype, '$inView', { value: inView })
+
+
 Vue.use(VueRouter)
 
 const app = new Vue({
