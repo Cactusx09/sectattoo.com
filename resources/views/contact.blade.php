64c8d71f3212960e3b4032c48298886646050e61
trg <cactusx09@gmail.com>
2020-03-17 22:05:48 +0200

    email template

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,13 @@
 
-<div class="well col-sm-8">
-    {{ $name }}
-    {{ $mail }}
-    {{ $text }}
+<style>
+    h3, div {
+        font-family: Arial;
+    }
+</style>
+<h3>This message from <b><a style="color: #cb1515" href="http://sectattoo.com" target="_blank">sectattoo.com</a></b></h3>
+<div>
+    <p><b>Name: </b>{{ $name }}</p>
+    <p><b>E-mail: </b><a href="mailto:{{ $mail }}">{{ $mail }}</a></p>
+    <b>Text: </b>
+    <p>{{ $text }}</p>
 </div>
